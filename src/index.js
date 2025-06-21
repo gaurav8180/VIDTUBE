862ff016301d4b52d5125ebc157f3ddcc7b2eacb
gaurav8180 <gauravsing0983@gmail.com>
2025-06-22 02:14:16 +0530

    code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello")
+import dotenv from "dotenv";
+dotenv.config(
+    {
+        path:"./.env"
+    }
+);
+import {app} from "./app.js";
+import connectDB from "./db/index.js"
+const PORT=process.env.PORT||3000
+
+connectDB().
+then(()=>{
+    app.listen (PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})})
+.catch((err)=>{
+    console.log("Mongodb connection error",err)
+})
