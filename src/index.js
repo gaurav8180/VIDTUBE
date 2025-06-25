402e2bc4989b3cc48ae434f7b62645411a123cb7
gaurav8180 <gauravsing0983@gmail.com>
2025-06-25 11:03:40 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {app} from "./app.js";
 import connectDB from "./db/index.js"
 const PORT=process.env.PORT||3000
 
-connectDB().
+connectDB().  
 then(()=>{
     app.listen (PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
